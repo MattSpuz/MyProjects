1abb1b9577b9e59693c2d409e47c92f1ea3b1bf1
MattSpuz <93402758+MattSpuz@users.noreply.github.com>
2022-05-10 19:00:46 -0500

    Using C#, ASP.NET. A Nutritional Calculator
    Uses SQL to store data and MVC to design the program. It allows the user to insert any type as many different foods and its nutritional facts(eg, calories, sugar, etc...). Its meant to represent what someone would eat in one day where you insert the nutritional facts of everything you've eaten and the program does the rest.  It also allows the user to add, edit, and delete their values which then is stored into the SQL database.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TempManager.Models;
+using System.Threading.Tasks;
+
+namespace Ch11Ex1TempManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TempManagerContext data { get; set; }
+        public HomeController(TempManagerContext ctx) => data = ctx;
+
+        public ViewResult Index()
+        {
+            var temps = data.Temps.OrderBy(t => t).ToList();
+            return View(temps);
+        }
+
+        [HttpGet]
+        public ViewResult Add() => View(new Temp());
+
+        [HttpPost]
+        public IActionResult Add(Temp temp)
+        {
+            if (ModelState.IsValid)
+            {
+                data.Temps.Add(temp);
+                data.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Please correct all errors.");
+                return View(temp);
+            }
+        }
+
+        [HttpGet]
+        public ViewResult Delete(int id)
+        {
+            var temp = data.Temps.Find(id);
+            return View(temp);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete(Temp temp)
+        {
+            data.Remove(temp);
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ViewResult DeleteAll()
+        {
+            var temps = data.Temps.OrderBy(t => t).ToList();
+            return View(temps);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult DeleteAllF()
+        {
+            var temps = data.Temps.OrderBy(t => t).ToList();
+            for (int i = temps.Count; i > 0; i--)
+            {
+                data.Remove(temps.ElementAt(i - 1));
+            }
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var temp = await data.Temps.FindAsync(id);
+            if (temp == null)
+            {
+                return NotFound();
+            }
+            return View(temp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Temp temp)
+        {
+            //add replace it
+            if (ModelState.IsValid)
+            {
+                data.Update(temp);
+                data.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            //change nothing and return if needed
+            else
+            {
+                ModelState.AddModelError("", "Please correct all errors.");
+                return View(temp);
+            }
+        }
+
+
+    }
+}
---
+++ Controllers/ValidationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TempManager.Models;
+
+namespace TempManager.Controllers
+{
+    public class ValidationController : Controller
+    {
+        private TempManagerContext data { get; set; }
+        public ValidationController(TempManagerContext ctx) => data = ctx;
+
+
+        public JsonResult CheckName(string date)
+        {
+            //DateTime dt = DateTime.Parse(date);
+            Temp temp = data.Temps.FirstOrDefault(t => t.FoodName == date);
+
+            if (temp == null)
+                return Json(true);
+            else
+                return Json($"{date} is already in the database");
+        }
+    }
+}
