0cfc9099bcc40980b800d12931b009b8ef682346
MattSpuz <93402758+MattSpuz@users.noreply.github.com>
2022-10-20 16:06:16 -0500

    Betting Game Using C# Forms
    Given nine randomized numbers, you must choose a range in which those numbers can earn points and increase your betting amount. More details are available within the program.

---
+++ RandomBettingGame/test3/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace test3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.RandomButton = new System.Windows.Forms.Button();
+            this.RandomNumberLabel = new System.Windows.Forms.Label();
+            this.RandomBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.BetAmountTextBox = new System.Windows.Forms.TextBox();
+            this.BetLabel = new System.Windows.Forms.Label();
+            this.BettingButton1 = new System.Windows.Forms.Button();
+            this.BettingButton2 = new System.Windows.Forms.Button();
+            this.BettingButton3 = new System.Windows.Forms.Button();
+            this.BettingButton4 = new System.Windows.Forms.Button();
+            this.ChosenBetTextBox = new System.Windows.Forms.TextBox();
+            this.ChosenBetLabel = new System.Windows.Forms.Label();
+            this.ResetPleaseLabel = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.ExplinationLabel = new System.Windows.Forms.Label();
+            this.GoodLuckLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(292, 438);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(168, 13);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Programmed By Matthew Spuzello";
+            // 
+            // RandomButton
+            // 
+            this.RandomButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RandomButton.Location = new System.Drawing.Point(277, 220);
+            this.RandomButton.Name = "RandomButton";
+            this.RandomButton.Size = new System.Drawing.Size(185, 63);
+            this.RandomButton.TabIndex = 1;
+            this.RandomButton.Text = "Randomize";
+            this.RandomButton.UseVisualStyleBackColor = true;
+            this.RandomButton.Click += new System.EventHandler(this.RandomButton_Click);
+            // 
+            // RandomNumberLabel
+            // 
+            this.RandomNumberLabel.AutoSize = true;
+            this.RandomNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RandomNumberLabel.Location = new System.Drawing.Point(218, 110);
+            this.RandomNumberLabel.Name = "RandomNumberLabel";
+            this.RandomNumberLabel.Size = new System.Drawing.Size(0, 39);
+            this.RandomNumberLabel.TabIndex = 2;
+            // 
+            // RandomBox
+            // 
+            this.RandomBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RandomBox.Location = new System.Drawing.Point(164, 113);
+            this.RandomBox.Name = "RandomBox";
+            this.RandomBox.ReadOnly = true;
+            this.RandomBox.Size = new System.Drawing.Size(146, 45);
+            this.RandomBox.TabIndex = 3;
+            this.RandomBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.RandomBox.TextChanged += new System.EventHandler(this.RandomBox_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(164, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(146, 45);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(164, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(146, 45);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(12, 11);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(146, 45);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(12, 62);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(146, 45);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(12, 113);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(146, 45);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(316, 11);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(146, 45);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(316, 62);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(146, 45);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(316, 113);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(146, 45);
+            this.textBox8.TabIndex = 11;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BetAmountTextBox
+            // 
+            this.BetAmountTextBox.Location = new System.Drawing.Point(163, 299);
+            this.BetAmountTextBox.Name = "BetAmountTextBox";
+            this.BetAmountTextBox.ReadOnly = true;
+            this.BetAmountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.BetAmountTextBox.TabIndex = 12;
+            this.BetAmountTextBox.Text = "100";
+            // 
+            // BetLabel
+            // 
+            this.BetLabel.AutoSize = true;
+            this.BetLabel.Location = new System.Drawing.Point(161, 275);
+            this.BetLabel.Name = "BetLabel";
+            this.BetLabel.Size = new System.Drawing.Size(62, 13);
+            this.BetLabel.TabIndex = 13;
+            this.BetLabel.Text = "Bet Amount";
+            // 
+            // BettingButton1
+            // 
+            this.BettingButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BettingButton1.Location = new System.Drawing.Point(12, 167);
+            this.BettingButton1.Name = "BettingButton1";
+            this.BettingButton1.Size = new System.Drawing.Size(146, 47);
+            this.BettingButton1.TabIndex = 14;
+            this.BettingButton1.Text = "1-25";
+            this.BettingButton1.UseVisualStyleBackColor = true;
+            this.BettingButton1.Click += new System.EventHandler(this.BettingButton1_Click);
+            // 
+            // BettingButton2
+            // 
+            this.BettingButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BettingButton2.Location = new System.Drawing.Point(164, 167);
+            this.BettingButton2.Name = "BettingButton2";
+            this.BettingButton2.Size = new System.Drawing.Size(146, 47);
+            this.BettingButton2.TabIndex = 15;
+            this.BettingButton2.Text = "26-50";
+            this.BettingButton2.UseVisualStyleBackColor = true;
+            this.BettingButton2.Click += new System.EventHandler(this.BettingButton2_Click);
+            // 
+            // BettingButton3
+            // 
+            this.BettingButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BettingButton3.Location = new System.Drawing.Point(316, 167);
+            this.BettingButton3.Name = "BettingButton3";
+            this.BettingButton3.Size = new System.Drawing.Size(146, 47);
+            this.BettingButton3.TabIndex = 16;
+            this.BettingButton3.Text = "51-75";
+            this.BettingButton3.UseVisualStyleBackColor = true;
+            this.BettingButton3.Click += new System.EventHandler(this.BettingButton3_Click);
+            // 
+            // BettingButton4
+            // 
+            this.BettingButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BettingButton4.Location = new System.Drawing.Point(12, 220);
+            this.BettingButton4.Name = "BettingButton4";
+            this.BettingButton4.Size = new System.Drawing.Size(146, 47);
+            this.BettingButton4.TabIndex = 17;
+            this.BettingButton4.Text = "76-100";
+            this.BettingButton4.UseVisualStyleBackColor = true;
+            this.BettingButton4.Click += new System.EventHandler(this.BettingButton4_Click);
+            // 
+            // ChosenBetTextBox
+            // 
+            this.ChosenBetTextBox.Location = new System.Drawing.Point(164, 247);
+            this.ChosenBetTextBox.Name = "ChosenBetTextBox";
+            this.ChosenBetTextBox.ReadOnly = true;
+            this.ChosenBetTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ChosenBetTextBox.TabIndex = 18;
+            // 
+            // ChosenBetLabel
+            // 
+            this.ChosenBetLabel.AutoSize = true;
+            this.ChosenBetLabel.Location = new System.Drawing.Point(164, 220);
+            this.ChosenBetLabel.Name = "ChosenBetLabel";
+            this.ChosenBetLabel.Size = new System.Drawing.Size(62, 13);
+            this.ChosenBetLabel.TabIndex = 19;
+            this.ChosenBetLabel.Text = "Chosen Bet";
+            // 
+            // ResetPleaseLabel
+            // 
+            this.ResetPleaseLabel.ForeColor = System.Drawing.Color.Firebrick;
+            this.ResetPleaseLabel.Location = new System.Drawing.Point(277, 290);
+            this.ResetPleaseLabel.Name = "ResetPleaseLabel";
+            this.ResetPleaseLabel.Size = new System.Drawing.Size(185, 30);
+            this.ResetPleaseLabel.TabIndex = 20;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetButton.Location = new System.Drawing.Point(12, 275);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(145, 44);
+            this.ResetButton.TabIndex = 21;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // ExplinationLabel
+            // 
+            this.ExplinationLabel.Location = new System.Drawing.Point(13, 326);
+            this.ExplinationLabel.Name = "ExplinationLabel";
+            this.ExplinationLabel.Size = new System.Drawing.Size(449, 97);
+            this.ExplinationLabel.TabIndex = 22;
+            this.ExplinationLabel.Text = resources.GetString("ExplinationLabel.Text");
+            // 
+            // GoodLuckLabel
+            // 
+            this.GoodLuckLabel.AutoSize = true;
+            this.GoodLuckLabel.Location = new System.Drawing.Point(13, 438);
+            this.GoodLuckLabel.Name = "GoodLuckLabel";
+            this.GoodLuckLabel.Size = new System.Drawing.Size(63, 13);
+            this.GoodLuckLabel.TabIndex = 23;
+            this.GoodLuckLabel.Text = "Good Luck!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(472, 460);
+            this.Controls.Add(this.GoodLuckLabel);
+            this.Controls.Add(this.ExplinationLabel);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.ResetPleaseLabel);
+            this.Controls.Add(this.ChosenBetLabel);
+            this.Controls.Add(this.ChosenBetTextBox);
+            this.Controls.Add(this.BettingButton4);
+            this.Controls.Add(this.BettingButton3);
+            this.Controls.Add(this.BettingButton2);
+            this.Controls.Add(this.BettingButton1);
+            this.Controls.Add(this.BetLabel);
+            this.Controls.Add(this.BetAmountTextBox);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.RandomBox);
+            this.Controls.Add(this.RandomNumberLabel);
+            this.Controls.Add(this.RandomButton);
+            this.Controls.Add(this.NameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Random Betting Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button RandomButton;
+        private System.Windows.Forms.Label RandomNumberLabel;
+        private System.Windows.Forms.TextBox RandomBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox BetAmountTextBox;
+        private System.Windows.Forms.Label BetLabel;
+        private System.Windows.Forms.Button BettingButton1;
+        private System.Windows.Forms.Button BettingButton2;
+        private System.Windows.Forms.Button BettingButton3;
+        private System.Windows.Forms.Button BettingButton4;
+        private System.Windows.Forms.TextBox ChosenBetTextBox;
+        private System.Windows.Forms.Label ChosenBetLabel;
+        private System.Windows.Forms.Label ResetPleaseLabel;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label ExplinationLabel;
+        private System.Windows.Forms.Label GoodLuckLabel;
+    }
+}
+
---
+++ RandomBettingGame/test3/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test3
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        int bet = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RandomBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Return a randomized number between 1 and 100
+        private int Randomizer()
+        {
+            int i;
+            i = rand.Next(1, 101);
+            return i;
+        }
+
+        //returns the number of points scored
+        private int CaldulatePoints()
+        {
+            int totalPoints = 0;
+            if(bet == 1)
+            {
+                if (1 <= Convert.ToInt16(RandomBox.Text) && Convert.ToInt16(RandomBox.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox1.Text) && Convert.ToInt16(textBox1.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox2.Text) && Convert.ToInt16(textBox2.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox3.Text) && Convert.ToInt16(textBox3.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox4.Text) && Convert.ToInt16(textBox4.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox5.Text) && Convert.ToInt16(textBox5.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox6.Text) && Convert.ToInt16(textBox6.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox7.Text) && Convert.ToInt16(textBox7.Text) <= 25)
+                    totalPoints++;
+                if (1 <= Convert.ToInt16(textBox8.Text) && Convert.ToInt16(textBox8.Text) <= 25)
+                    totalPoints++;
+            }
+            else if (bet == 2)
+            {
+                if (26 <= Convert.ToInt16(RandomBox.Text) && Convert.ToInt16(RandomBox.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox1.Text) && Convert.ToInt16(textBox1.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox2.Text) && Convert.ToInt16(textBox2.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox3.Text) && Convert.ToInt16(textBox3.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox4.Text) && Convert.ToInt16(textBox4.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox5.Text) && Convert.ToInt16(textBox5.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox6.Text) && Convert.ToInt16(textBox6.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox7.Text) && Convert.ToInt16(textBox7.Text) <= 50)
+                    totalPoints++;
+                if (26 <= Convert.ToInt16(textBox8.Text) && Convert.ToInt16(textBox8.Text) <= 50)
+                    totalPoints++;
+            }
+            else if (bet == 3)
+            {
+                if (51 <= Convert.ToInt16(RandomBox.Text) && Convert.ToInt16(RandomBox.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox1.Text) && Convert.ToInt16(textBox1.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox2.Text) && Convert.ToInt16(textBox2.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox3.Text) && Convert.ToInt16(textBox3.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox4.Text) && Convert.ToInt16(textBox4.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox5.Text) && Convert.ToInt16(textBox5.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox6.Text) && Convert.ToInt16(textBox6.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox7.Text) && Convert.ToInt16(textBox7.Text) <= 75)
+                    totalPoints++;
+                if (51 <= Convert.ToInt16(textBox8.Text) && Convert.ToInt16(textBox8.Text) <= 75)
+                    totalPoints++;
+            }
+            else if (bet == 4)
+            {
+                if (76 <= Convert.ToInt16(RandomBox.Text) && Convert.ToInt16(RandomBox.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox1.Text) && Convert.ToInt16(textBox1.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox2.Text) && Convert.ToInt16(textBox2.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox3.Text) && Convert.ToInt16(textBox3.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox4.Text) && Convert.ToInt16(textBox4.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox5.Text) && Convert.ToInt16(textBox5.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox6.Text) && Convert.ToInt16(textBox6.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox7.Text) && Convert.ToInt16(textBox7.Text) <= 100)
+                    totalPoints++;
+                if (76 <= Convert.ToInt16(textBox8.Text) && Convert.ToInt16(textBox8.Text) <= 100)
+                    totalPoints++;
+            }
+            return totalPoints;
+        }
+
+        //Determaines the players points scored and total bet
+        private void RandomButton_Click(object sender, EventArgs e)
+        {
+            if(BetAmountTextBox.Text != "You Lose" && bet != 0)
+            {
+                ResetPleaseLabel.Text = "";
+                RandomBox.Text = Randomizer().ToString();
+                textBox1.Text = Randomizer().ToString();
+                textBox2.Text = Randomizer().ToString();
+                textBox3.Text = Randomizer().ToString();
+                textBox4.Text = Randomizer().ToString();
+                textBox5.Text = Randomizer().ToString();
+                textBox6.Text = Randomizer().ToString();
+                textBox7.Text = Randomizer().ToString();
+                textBox8.Text = Randomizer().ToString();
+
+                BetAmountTextBox.Text = (CaldulatePoints() * Convert.ToInt64(BetAmountTextBox.Text)).ToString();
+                if (BetAmountTextBox.Text == "0")
+                {
+                    BetAmountTextBox.Text = "You Lose";
+                }
+            }
+            else
+            {
+                ResetPleaseLabel.Text = "You've either lost or haven't picked a bet";
+            }
+        }
+
+        //Changes bet to 1-25
+        private void BettingButton1_Click(object sender, EventArgs e)
+        {
+            bet = 1;
+            ChosenBetTextBox.Text = "1-25";
+        }
+
+        //Changes bet to 26-50
+        private void BettingButton2_Click(object sender, EventArgs e)
+        {
+            bet = 2;
+            ChosenBetTextBox.Text = "26-50";
+        }
+
+        //Changes bet to 51-75
+        private void BettingButton3_Click(object sender, EventArgs e)
+        {
+            bet = 3;
+            ChosenBetTextBox.Text = "51-75";
+        }
+
+        //Changes bet to 76-100
+        private void BettingButton4_Click(object sender, EventArgs e)
+        {
+            bet = 4;
+            ChosenBetTextBox.Text = "76-100";
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            bet = 0;
+            ChosenBetTextBox.Text = "";
+            BetAmountTextBox.Text = "100";
+        }
+    }
+}
