e5f4b29bb79ad2cef2f2d76387f31874c75105a6
MattSpuz <93402758+MattSpuz@users.noreply.github.com>
2023-03-22 20:47:42 -0500

    Model for new database
    The database saves all items the user adds for when they want to quickly reinsert something they previously deleted.

---
+++ Diet_Manager_Program/Models/Histories.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using Newtonsoft.Json;
+using System.Web;
+using System.Collections.Generic;
+
+namespace TempManager.Models
+{
+    public class Histories
+    {
+        public int Id { get; set; }
+
+        public string FoodName { get; set; }
+
+        public double? Calories { get; set; }
+
+        public double? Sugar { get; set; }
+
+        public double? Fat { get; set; }
+
+        public double? Sodium { get; set; }
+
+        public double? Protein { get; set; }
+    }
+}
